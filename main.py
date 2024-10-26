92148e9d0d86b4f9faad83f20b105bf6b3b9a4ef
Dmitriy Kuznetsov <dimaric.cool@gmail.com>
2024-10-26 19:23:13 +0300

    отрисовка квадрата

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ Screen().setup(800, 700)
 
 
 shape("turtle")
-speed(10)
+speed(3)
 shapesize(1.5)
 
+pensize(10)
+
+pendown()
+for _ in range(4):
+    forward(100)
+    right(90)
+penup()
 
 mainloop()
