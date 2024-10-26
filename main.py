031d25429e2d86b05166b181aab6a2f9646dae52
Dmitriy Kuznetsov <dimaric.cool@gmail.com>
2024-10-26 19:41:35 +0300

    добавил отрисовку квадратов

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import *
 
-Screen().setup(800, 700)
+WIDTH = 800
+HEIGHT = 700
+
+Screen().setup(WIDTH, HEIGHT)
 
 
 shape("turtle")
@@ -8,11 +11,21 @@ speed(3)
 shapesize(1.5)
 
 pensize(10)
-
-pendown()
-for _ in range(4):
-    forward(100)
-    right(90)
 penup()
+
+dlinaSlova = 3
+
+X = -250
+Y = -200
+
+for x in range(dlinaSlova):
+    goto(X, Y)
+    pendown()
+    for _ in range(4):
+        forward(50)
+        right(90)
+    penup()
+    X += 50 + 20
+
 
 mainloop()
