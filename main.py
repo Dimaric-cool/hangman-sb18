57a939e17073cce293a8868bfb03c9cd0e44402e
Dmitriy Kuznetsov <dimaric.cool@gmail.com>
2024-11-23 18:11:27 +0300

    добавили обработку клика по буквам

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+import turtle
 from turtle import *
 
 WIDTH = 800
@@ -6,26 +8,12 @@ HEIGHT = 700
 Screen().setup(WIDTH, HEIGHT)
 
 shape("turtle")
-speed(3)
+speed(0)
 shapesize(1.5)
-
+fontSize = 42
 pensize(10)
 penup()
-dlinaSlova = 4
-dlinakwadr = 50
-otstup = 20
-X = - (dlinaSlova * dlinakwadr + (dlinaSlova - 1) * otstup) // 2
-Y = -250
 
-
-for x in range(dlinaSlova):
-    goto(X, Y)
-    pendown()
-    for _ in range(4):
-        forward(dlinakwadr)
-        right(90)
-    penup()
-    X += dlinakwadr + otstup
 
 
 def draw_line(x1, y1, x2, y2):
@@ -60,5 +48,52 @@ def draw_error(numError):
         case 10:
             draw_line(-220, 70, -310, 120)
 
+def draw_squares(dlinaSlova):
+    dlinakwadr = 50
+    otstup = 20
+    X = - (dlinaSlova * dlinakwadr + (dlinaSlova - 1) * otstup) // 2
+    Y = -250
 
+    for x in range(dlinaSlova):
+        goto(X, Y)
+        pendown()
+        for _ in range(4):
+            forward(dlinakwadr)
+            right(90)
+        penup()
+        X += dlinakwadr + otstup
+
+
+def draw_alphabet(coords, fontSize):
+    X, Y = coords
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    alphaCoords = {}
+    for letter in alphabet:
+        alphaCoords[letter] = (X, Y)
+        goto(X, Y)
+        write(letter, False, "center", ("Comic Sans MS", fontSize, "bold"))
+        X += 60
+        if X >= 350:
+            X = 70
+            Y -= 65
+    return alphaCoords
+
+def get_coord(x, y):
+    click_check((x, y), alphaCoords)
+
+def click_check(coord, dicti):
+    d = fontSize // 2
+    for key, value in dicti.items():
+        if abs(coord[0] - value[0]) <= d and abs((coord[1]) - (value[1] + d)) <= d :
+            print(key)
+
+draw_squares(6)
+
+for error in range(1, 11):
+    draw_error(error)
+
+alphaCoords = draw_alphabet((70, 210), fontSize)
+
+
+Screen().onclick(get_coord)
 mainloop()
